2d4646b79c875d455452c33988d0c19ba8385beb
Simon Bischof <uedkm@student.kit.edu>
2012-01-03 22:23:46 +0100

    Added SMTLibTranslator stub, changed output in VerifierInterface to English

--- src/pse11/Main.java
+++
@@ -1,8 +0,0 @@
-import gui.controller.MainController;
-
-public class Main {
-    
-    public static void main(String[] args) {
-        new MainController();
-    }
-}
--- src/pse11/verifier/VerifierInterface.java
+++ src/pse11/verifier/VerifierInterface.java
@@ -15,10 +15,10 @@ import java.io.InputStreamReader;
  */
 public class VerifierInterface {
     private WPProgram program;
-    private interpreter.SMTLibTranslator smtLibTranslator;
+    private SMTLibTranslator smtLibTranslator;
     private String verifier;
 
-    public VerifierInterface(interpreter.SMTLibTranslator smtLibTranslator, String verifier) {
+    public VerifierInterface(SMTLibTranslator smtLibTranslator, String verifier) {
         this.smtLibTranslator = smtLibTranslator;
         this.verifier = verifier;
     }
@@ -28,7 +28,7 @@ public class VerifierInterface {
     }
 
     public String verify(ASTRoot ast) throws IOException {
-        program = interpreter.SMTLibTranslator.getWPTree(ast);
+        program = smtLibTranslator.getWPTree(ast);
         File file = saveInSMTFile(program.toString());
         Process verifierProcess = Runtime.getRuntime().exec(verifier + " text.smt2");
         InputStream verifierout = verifierProcess.getInputStream();
@@ -37,7 +37,7 @@ public class VerifierInterface {
         try {
             verifierProcess.waitFor();
         } catch (InterruptedException ex) {
-            System.out.println("Beweiser wurde unerwartet beendet.");
+            System.out.println("Verifier was interrupted unexpectedly.");
         }
         StringBuilder output = new StringBuilder("");
         int lastRead = 0;
