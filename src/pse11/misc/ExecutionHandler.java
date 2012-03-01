19731d70e82d7686d6f7f1f7be32cdc5035b961e
Lin Jin <Lin@VAIOlin.(none)>
2012-03-01 17:40:28 +0100

    Fixed #76, added example test with arrays

--- src/pse11/misc/ExecutionHandler.java
+++ src/pse11/misc/ExecutionHandler.java
@@ -45,6 +45,8 @@ public class ExecutionHandler {
 
 	public void parse(String source) {
 		this.messagesystem.clear(MessageCategories.ERROR);
+		this.messagesystem.clear(MessageCategories.VERIFYERROR);
+		this.messagesystem.clear(MessageCategories.MISC);
 		try {
 			this.ast = this.parser.parseProgram(source);
 		} catch (RecognitionException re) {
@@ -116,7 +118,9 @@ public class ExecutionHandler {
 	}
 	
 	public void verify(String source) {
-        this.messagesystem.clear(MessageCategories.VERIFYERROR);
+		this.messagesystem.clear(MessageCategories.ERROR);
+		this.messagesystem.clear(MessageCategories.VERIFYERROR);
+		this.messagesystem.clear(MessageCategories.MISC);
 		this.parse(source);
 		if(this.ast == null) {
 			return;
