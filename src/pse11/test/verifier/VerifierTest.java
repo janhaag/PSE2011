84d64c765e0a28fd4f181a02dbd4b8780b89c2e3
Lin Jin <Lin@VAIOlin.(none)>
2012-03-07 19:24:29 +0100

    Added unit tests for verifier package

--- src/pse11/test/verifier/VerifierTest.java
+++ src/pse11/test/verifier/VerifierTest.java
@@ -18,16 +18,22 @@ import ast.Program;
 import ast.StatementBlock;
 import ast.VariableDeclaration;
 
+import verifier.VerifierInterface;
 import verifier.smtlib.z3.Z3;
 
 public class VerifierTest {
 	private Z3 z3;
+	private Z3 z4;
+	private VerifierInterface verifier;
 
 	@Before
 	public void setUp() {
-		this.z3 = new Z3("hallo");
+		this.z3 = new Z3("${FILE} ${MEMORY_LIMIT} ${TIMEOUT}");
+		this.z4 = new Z3("hallo");
+		this.verifier = new VerifierInterface(this.z3);
 	}
 
+	/* Z3 tests */
 	@Test
 	public void getDescriptionsTest() {
 		assertNull(this.z3.getDescriptions());
@@ -39,6 +45,20 @@ public class VerifierTest {
 	}
 	
 	@Test
+	public void setAndGetTimeoutTest() {
+		this.z3.setTimeout("10");
+		assertEquals("10", this.z3.getTimeout());
+	}
+	
+	@Test
+	public void setAndGetMemoryLimitTest() {
+		this.z3.setMemoryLimit("100");
+		assertEquals("100", this.z3.getMemoryLimit());
+	}
+	
+	
+	/* Verifier test */
+	@Test
 	public void callTest() {
 		Position pos = new Position(1, 0);
 		Program ast = new Program(pos, new Function[0], new Function(
@@ -46,16 +66,42 @@ public class VerifierTest {
                 new StatementBlock(new VariableDeclaration[0], pos), new Assumption[0],
                 new Ensure[0]), null);
 		try {
-			String s = this.z3.call(ast);
-			assertEquals("", s);
-		} catch (IOException e) {
+			assertEquals("", this.z4.call(ast));
+		} catch (IOException expected) {
 		} catch (InterruptedException e) {
 		}
+		
+		try {
+			this.z3.setTimeout("10");
+			this.z3.setMemoryLimit("100");
+			this.z3.call(ast);
+		} catch (IOException expected) {
+		} catch (InterruptedException e) {
+		}
+	}
+	
+	/* VerifierInterface tests */
+	@Test
+	public void setVerifierTimeoutTest() {
+		this.verifier.setTimeout("42");
+		assertEquals("42", this.z3.getTimeout());
+	}
+	
+	@Test
+	public void setVerifierMemorylimitTest() {
+		this.verifier.setMemoryLimit("10");
+		assertEquals("10", this.z3.getMemoryLimit());
+	}
+	
+	@Test
+	public void getVerifierDescriptionsTest() {
+		assertNull(this.verifier.getDescriptions());
 	}
 	
 	@After
 	public void tearDown() {
 		this.z3 = null;
+		this.verifier = null;
 	}
 
 }
